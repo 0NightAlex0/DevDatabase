ab280ed86426d4c9d18c6d2d1a03b771052c92b0
rubenev <mac_fley@hotmail.com>
2016-10-19 16:32:00 +0200

    syntax

--- database_assignment2.js
+++ database_assignment2.js
@@ -3,14 +3,14 @@
 		[
 		{"e_bsn": 1222, "Name":"Ruben","Surname":"Everwijn","work_building":"H-Gebouw",
 		 "address":
-			["country": "Nederland","Postal_code":"3201TL","City":"Spijkenisse",
+			[{"country": "Nederland","Postal_code":"3201TL","City":"Spijkenisse",
 			"Street":"Wagenmaker","house_nr":25},
-			"country": "Nederland","Postal_code":"3201RR","City":"Spijkenisse",
-			"Street":"Slaanbreek","house_nr":126}]
-		{"e_bsn": 1223, "Name":"Alexander","Surname":"Ng","work_building":"B-Gebouw"}
+			{"country": "Nederland","Postal_code":"3201RR","City":"Spijkenisse",
+		"Street":"Slaanbreek","house_nr":126}]},
+		{"e_bsn": 1223, "Name":"Alexander","Surname":"Ng","work_building":"B-Gebouw",
 		 "address":
-			["country": "Nederland","Postal_code":"3101GG","City":"Rotterdam",
-			"Street":"Hooivorklaan","house_nr":2}]
+			[{"country": "Nederland","Postal_code":"3101GG","City":"Rotterdam",
+			"Street":"Hooivorklaan","house_nr":2}]}
 		]
 }// Deze database werkt niet met primary keys en foreignkeys 
 //  dus alle info over een employee plaats je in employee
