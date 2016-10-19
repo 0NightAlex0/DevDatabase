d4e38b002a02d7b848df717f9b4a896d3d83032d
rubenev <mac_fley@hotmail.com>
2016-10-19 16:36:41 +0200

    position project

--- database_assignment2.js
+++ database_assignment2.js
@@ -6,7 +6,12 @@
 			[{"country": "Nederland","Postal_code":"3201TL","City":"Spijkenisse",
 			"Street":"Wagenmaker","house_nr":25},
 			{"country": "Nederland","Postal_code":"3201RR","City":"Spijkenisse",
-		"Street":"Slaanbreek","house_nr":126}]},
+		"Street":"Slaanbreek","house_nr":126}],
+		"Position_work": "Manager",
+		"Position_project":
+			[{"p_id": "P22","position_project":"Scrummaster"}]
+			},
+		
 		{"e_bsn": 1223, "Name":"Alexander","Surname":"Ng","work_building":"B-Gebouw",
 		 "address":
 			[{"country": "Nederland","Postal_code":"3101GG","City":"Rotterdam",
