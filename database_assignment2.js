a9a056b883c1190670fc34198c6506e23fda4d41
rubenev <mac_fley@hotmail.com>
2016-10-19 16:19:07 +0200

    een voorproefje

---
+++ database_assignment2.js
@@ -0,0 +1,15 @@
+{ 
+	"employee":
+		[
+		{"e_bsn": 1222, "Name":"Ruben","Surname":"Everwijn","work_building":"H-Gebouw",
+		 "address":
+			["country": "Nederland","Postal_code":"3201TL","City":"Spijkenisse",
+			"Street":"Wagenmaker","house_nr":25},
+			"country": "Nederland","Postal_code":"3201RR","City":"Spijkenisse",
+			"Street":"Slaanbreek","house_nr":126}]
+		{"e_bsn": 1223, "Name":"Alexander","Surname":"Ng","work_building":"B-Gebouw"}
+		 "address":
+			["country": "Nederland","Postal_code":"3101GG","City":"Rotterdam",
+			"Street":"Hooivorklaan","house_nr":2}}
+		]
+}
