d5aa74d40c7cd2667aa506bad260dcc5a8099e6d
rubenev <mac_fley@hotmail.com>
2016-10-23 21:00:10 +0200

    upload

--- assignment2/MongoDB/src/mongodbtest/MongoDBTest.java
+++ assignment2/MongoDB/src/mongodbtest/MongoDBTest.java
@@ -1,31 +1,25 @@
 
 package mongodbtest;
 import com.mongodb.BasicDBObject;
-import com.mongodb.BulkWriteOperation;
-import com.mongodb.BulkWriteResult;
-import com.mongodb.Cursor;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.ParallelScanOptions;
 import com.mongodb.util.JSON;
+import java.lang.Thread.State;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.function.Function;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  *
  * @author Alex
  */
-public class MongoDBTest {
+public class MongoDBTest<T>{
 
 
     public static void main(String[] args) throws UnknownHostException {
@@ -34,14 +28,15 @@ public class MongoDBTest {
         List<String> listName = new ArrayList<>(Arrays.asList("Peter","Jay","Mike","Sally","Michel","Bob","Jenny"));
         List<String> listSurname = new ArrayList<>(Arrays.asList("Ra","Grey","Uks","Bla","Woo","Foo","Lu"));
         List<String> listOccupation = new ArrayList<>(Arrays.asList("Analyst","Developer","Tester"));
-        List<String> listHours = new ArrayList<>(Arrays.asList("4","5","6","19","20","21"));
+        List<Integer> listHours = new ArrayList<>(Arrays.asList(4,5,6,19,20,21));
+        
         for(int i = 1000; i < 2000;i++){
             	String json = "{'e_bsn': '" + Integer.toString(i) + "', 'Name':'" + getRandomItem(listName) +"','Surname':'" + getRandomItem(listSurname) +"'"
                     + ",'building_name':'H-Gebouw'"
                     + ",'address':[{'country': 'Nederland','Postal_code':'3201TL','City':'Spijkenisse','Street':'Wagenmaker','house_nr':25},"
                     + "{'country': 'Nederland','Postal_code':'3201RR','City':'Spijkenisse','Street':'Slaanbreek','house_nr':126}],"
-                    + "'Position_project': [{'p_id': 'P" + Integer.toString(i) + "','position_project':'"+ getRandomItem(listOccupation)
-                    +"', 'worked_hours':"+ Integer.parseInt(getRandomItem(listHours)) +"}],"
+                    + "'Position_project': [{'p_id': 'P" + Integer.toString(i%100) + "','position_project':'"+ getRandomItem(listOccupation)
+                    +"', 'worked_hours':"+ getRandomItem(listHours) +"}],"
                     + "'degree_employee': [{'Course':'Informatica','School':'HogeSchool Rotterdam','Level':'Bachelorr'}]}";
                 
                 DBObject dbObject = (DBObject)JSON.parse(json);//parser
@@ -59,8 +54,15 @@ public class MongoDBTest {
                 BasicDBObject oWQuery = new BasicDBObject();
                 oWQuery.put("Position_project.worked_hours", new BasicDBObject("$gt", 20));//overwork
                 
-                DBCursor cursorDocJSON = colEmployee.find(oWQuery,fields); //get documents
+                BasicDBObject pidQuery = new BasicDBObject();
+                pidQuery.put("Position_project.p_id", new BasicDBObject("$eq", "P20"));//work in project     
+
+                BasicDBObject hourQuery = new BasicDBObject();
+                hourQuery.put("Position_project.worked_hours", new BasicDBObject("$eq", 20));//overwork   
+                
+                DBCursor cursorDocJSON = colEmployee.find(hourQuery,fields); //get documents
             while (cursorDocJSON.hasNext()) {
+                   
                    System.out.println(cursorDocJSON.next());
             }
             colEmployee.remove(new BasicDBObject());
@@ -71,4 +73,31 @@ public class MongoDBTest {
     static <T> T getRandomItem(List<T> list) {
         return list.get(rand.nextInt(list.size()));
     }
+
 }
+//List<Student> students = new ArrayList<Student>();
+//
+//BasicDBObject query = new BasicDBObject();
+//query.put("user", username); 
+//DBCursor cursor = theCollection.find(query); 
+//while (cursor.hasNext()) {
+//    DBObject theObj = cursor.next();
+//    //How to get the DBObject value to ArrayList of Java Object?
+//
+//    BasicDBList studentsList = (BasicDBList) theObj.get("students");
+//    for (int i = 0; i < studentsList.size(); i++) {
+//        BasicDBObject studentObj = (BasicDBObject) studentsList.get(i);
+//        String firstName = studentObj.getString("firstName");
+//        String lastName = studentObj.getString("lastName");
+//        String age = studentObj.getString("age");
+//        String gender = studentObj.getString("gender");
+//
+//        Student student = new Student();
+//        student.setFirstName(firstName);
+//        student.setLastName(lastName);
+//        student.setAge(age);
+//        student.setGender(gender);
+//
+//        students.add(student);
+//    }               
+//}
